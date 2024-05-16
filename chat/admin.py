f82bbc01847f0d88ef74204bc11826adaa4230ee
Ferven <120587289+fervenmusic@users.noreply.github.com>
2024-05-16 17:46:03 +0200

    final changes

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Message, Chat
 
-# Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    field = ('chat', 'text', 'created_at', 'author', 'receiver')
+    list_display = ('created_at', 'author', 'text', 'receiver')
+    search_fields = ('text',)
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from .models import Message, Chat
+from django.http.response import HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.core import serializers
 
-# Create your views here.
+
+@login_required(login_url='/login/')
+def index(request):
+    if request.method == 'POST':
+        print("Received data " + request.POST['textmessage'])
+        myChat = Chat.objects.get(id=1)
+        new_Message = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        serialized_obj = serializers.serialize('json', [ new_Message])
+        return JsonResponse(serialized_obj[1:-1], safe=False)
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages': chatMessages})
+
+def login_view(request):
+    redirect = request.GET.get('next')
+    if request.method == 'POST':
+        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(request.POST.get('redirect'))
+        else:
+            return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})
+
+    return render(request, 'auth/login.html', {'redirect': redirect})
